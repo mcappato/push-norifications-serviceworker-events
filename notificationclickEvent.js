5eb6a08f3f97d0500d462a237200e019c39bb543
mcappato <mcappato@makingsense.com>
2022-03-02 12:29:23 -0300

    storage scripting into const

--- notificationclickEvent.js
+++ notificationclickEvent.js
@@ -1,8 +1,14 @@
+console.log("hello");
 
+const notificationClickEvent = (event) => {
   console.log('se dispara al hacer click en push');
+
+  console.log(event);
 
   fetch('https://api.github.com/users/manishmshiva')
     .then(response => response.json())
     .then(json => console.log(json))
     .catch(err => console.log('Request Failed', err));
+}
+  
  
